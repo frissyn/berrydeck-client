48ba030546f41bdc7edc458aa779c84ae5d82a73
frissyn <62220201+frissyn@users.noreply.github.com>
2022-04-28 14:46:09 +0000

    :sparkles: add index page, wip

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<script setup>
+const editor = useEditor();
+</script>
+
+<template>
+<div class="navbar bg-base-100 shadow-md">
+  <div class="flex-1">
+    <a class="btn btn-ghost normal-case text-xl">BerryDeck</a>
+  </div>
+</div>
+
+<div class="drawer drawer-mobile drawer-end">
+  <input id="main-drawer" type="checkbox" class="drawer-toggle">
+  <div class="drawer-content flex flex-col items-center justify-center">
+    <div class="hero min-h-screen bg-base-200">
+      <div class="hero-content text-center">
+      <div class="max-w-md">
+        <p class="py-6">coming soon? currently editing? {{ editor.open }}</p>
+        <div class="btn-group">
+          <button class="btn btn-active" @click="!editor.open"><IconsPlus /> New Savefile</button>
+          <button class="btn"><IconsFolder /> Open Savefile</button>
+        </div>
+      </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="drawer-side">
+    <label for="main-drawer" class="drawer-overlay"></label>
+    <ul class="menu overflow-y-auto w-80 bg-base-100 text-base-content">
+      <li class="bordered"><a>Sidebar Item 1</a></li>
+      <li><a>Sidebar Item 2</a></li>
+    </ul>
+  </div>
+</div>
+</template>
